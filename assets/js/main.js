d3772ad117b1cb35d9cef4bcf77031f4eff7f6f7
SLautaro <santillanlautaronahuel@gmail.com>
2023-01-06 07:09:42 -0300

    retoques

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,6 @@ let allEvents = [].concat(data.events)
 let pastEvents = []
 let upcomingEvents = []
 let arrEvents = generateCards();
-console.log(generateCards())
-console.log(arrEvents)
-console.log(document.location.pathname)
-
-
 
 /*   FILTER BY DATE*/
 function filterEventsDate(eventObj) {
@@ -18,15 +13,95 @@ function filterEventsDate(eventObj) {
             upcomingEvents.push(event)
         }
     }
-
 }
-console.log(allEvents, pastEvents, upcomingEvents)
+
+
+/* GENERATE CARDS ACCORDING TO PAGE */
+filterEventsDate(data)
+function generateCards() {
+    if (document.location.pathname == "/index.html") {
+        return allEvents
+    } else if (document.location.pathname == "/past_events.html") {
+        return pastEvents
+    } else if (document.location.pathname == "/upcoming_events.html") {
+        return upcomingEvents
+
+    }
+}
 
 let sectionCards = document.getElementById("aevents")
+const searchBar = document.getElementById("search-bar")
+const checkBoxDiv = document.getElementById("check-search")
 
 
+checkBoxDiv.addEventListener('change', crossFilter)
+searchBar.addEventListener('input', crossFilter)
+document.addEventListener("DOMContentLoaded", function () {
+    renderCards(generateCards(), sectionCards);
+});
 
-/*    TEMPLATE    */
+
+const noRepeatCategories = Array.from(new Set(data.events.map(e => e.category)))
+checkBoxDiv.innerHTML = generateCheckbox(noRepeatCategories)
+
+
+function textSearch(input) {
+    let textFilter = arrEvents.filter(event => event.name.toLowerCase().includes(input.value.toLowerCase()))
+    return textFilter
+}
+
+function checkSearch(arr) {
+    let checkFilter;
+    const checkBox = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'));
+    let arrCheckBox = checkBox.map(i => i.value)
+    checkFilter = arr.filter(event => arrCheckBox.includes(event.category))
+    return checkFilter
+}
+
+
+function crossFilter() {
+    const checkBoxcross = Array.from(document.querySelectorAll('input[type="checkbox"]'))
+    if (searchBar.value !== "" && checkBoxcross.some(c => c.checked)) {
+        let filteredSearch = textSearch(searchBar)
+        let filteredCheck = checkSearch(filteredSearch)
+        renderCards(filteredCheck, sectionCards)
+        if (filteredCheck.length === 0) {
+            let noRes = `<h2 class="alert">No results available :(   ...</h2>`
+            generateNoResults(noRes)
+        }
+    } else if (searchBar.value !== "" && checkBoxcross.every(c => !c.checked)) {
+        let filteredSearch = textSearch(searchBar)
+        renderCards(filteredSearch, sectionCards)
+        if (filteredSearch.length === 0) {
+            let noRes = `<h2 class="alert">No results available :(   ...</h2>`
+            generateNoResults(noRes)
+        }
+    } else if (searchBar.value === "" && checkBoxcross.some(c => c.checked)) {
+        let filteredCheck = checkSearch(arrEvents)
+        renderCards(filteredCheck, sectionCards)
+        if (filteredCheck.length === 0) {
+            let noRes = `<h2 class="alert">No results available :(   ...</h2>`
+            generateNoResults(noRes)
+        }
+    }
+    else {
+        renderCards(arrEvents, sectionCards)
+    }
+}
+
+function renderCards(arr, place) {
+    place.innerHTML = generateTemplate(arr)
+}
+
+function renderCardsCheck(arr, place) {
+    place.innerHTML += generateTemplate(arr)
+}
+
+function generateNoResults(msg) {
+    sectionCards.innerHTML = msg
+}
+
+/*    TEMPLATES    */
 
 function generateTemplate(eventObj) {
     let template = ''
@@ -50,107 +125,6 @@ function generateTemplate(eventObj) {
     }); return template
 }
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    renderCards(generateCards(), sectionCards);
-});
-
-/* GENERATE CARDS ACCORDING TO PAGE */
-
-filterEventsDate(data)
-
-
-
-function generateCards() {
-    if (document.location.pathname == "/index.html") {
-        return allEvents
-    } else if (document.location.pathname == "/past_events.html") {
-        return pastEvents
-    } else if (document.location.pathname == "/upcoming_events.html") {
-        return upcomingEvents
-
-    }
-}
-
-
-function renderCards(arr, place) {
-    place.innerHTML = generateTemplate(arr)
-}
-
-function renderCardsCheck(arr, place) {
-    place.innerHTML += generateTemplate(arr)
-}
-
-function generateNoResults(msg){
-    sectionCards.innerHTML=msg
-}
-
-
-
-/* ---------------------TASK            3 ------------------------*/
-
-const searchBar = document.getElementById("search-bar")
-const checkBoxDiv = document.getElementById("check-search")
-checkBoxDiv.addEventListener('change', crossFilter)
-
-
-
-
-
-searchBar.addEventListener('input', crossFilter)
-console.log(searchBar.value)
-
-function textSearch(input) {
-    let textFilter = arrEvents.filter(event => event.name.toLowerCase().includes(input.value.toLowerCase()))
-    return textFilter
-}
-
-function checkSearch(arr) {
-    var checkFilter;/* PIDO PERDON PADRE PUES HE PECADO */ 
-    const checkBox =Array.from(document.querySelectorAll( 'input[type="checkbox"]:checked' ));
-    let arrCheckBox = checkBox.map(i=>i.value)
-        checkFilter = arr.filter(event => arrCheckBox.includes(event.category))
-    
-    console.log(arrCheckBox)
-    return checkFilter  
-}
-
-
-function crossFilter(){
-    const checkBoxcross =Array.from(document.querySelectorAll( 'input[type="checkbox"]' ))
-    if(searchBar.value!=="" && checkBoxcross.some(c=>c.checked)){
-            let filteredSearch=textSearch(searchBar)
-            let filteredCheck=checkSearch(filteredSearch)
-            renderCards(filteredCheck, sectionCards)
-            if(filteredCheck.length===0){
-                let noRes=`<h2 class="alert">No results available :(   ...</h2>`
-                generateNoResults(noRes)
-        }
-        
-    }else if(searchBar.value!=="" && checkBoxcross.every(c=>!c.checked)){
-        let filteredSearch=textSearch(searchBar)
-    renderCards(filteredSearch, sectionCards)
-    if(filteredSearch.length===0){
-        let noRes=`<h2 class="alert">No results available :(   ...</h2>`
-        generateNoResults(noRes)
-}
-    }else if (searchBar.value==="" && checkBoxcross.some(c=>c.checked)){
-        let filteredCheck=checkSearch(arrEvents)
-        renderCards(filteredCheck, sectionCards)
-        if(filteredCheck.length===0){
-            let noRes=`<h2 class="alert">No results available :(   ...</h2>`
-            generateNoResults(noRes)
-    }
-    }
-    else {
-        renderCards(arrEvents, sectionCards)
-    }
-  }  
-
-const noRepeatCategories = Array.from(new Set(data.events.map(e => e.category)))
-
-
-/*  */
 function generateCheckbox(arr) {
     let templateCheck = ""
     arr.forEach(category => {
@@ -162,6 +136,5 @@ function generateCheckbox(arr) {
     })
     return templateCheck
 }
-checkBoxDiv.innerHTML = generateCheckbox(noRepeatCategories)
-/*  */
+
 
